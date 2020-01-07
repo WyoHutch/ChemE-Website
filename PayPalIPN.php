33134c86fa2a48eafdfb1837e0b74a86bbca64d7
WyoHutch <55603181+WyoHutch@users.noreply.github.com>
2020-01-07 14:14:21 -0700

    Update to downloads tables

---
+++ PayPalIPN.php
@@ -0,0 +1,151 @@
+<?php
+
+// STEP 1: read POST data
+
+// Reading POSTed data directly from $_POST causes serialization issues with array data in the POST.
+// Instead, read raw POST data from the input stream.
+$raw_post_data = file_get_contents('php://input');
+$raw_post_array = explode('&', $raw_post_data);
+$myPost = array();
+foreach ($raw_post_array as $keyval) {
+    $keyval = explode('=', $keyval);
+    if (count($keyval) == 2)
+        $myPost[$keyval[0]] = urldecode($keyval[1]);
+}
+// read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+$req = 'cmd=_notify-validate';
+if (function_exists('get_magic_quotes_gpc')) {
+    $get_magic_quotes_exists = true;
+}
+foreach ($myPost as $key => $value) {
+    if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+        $value = urlencode(stripslashes($value));
+    } else {
+        $value = urlencode($value);
+    }
+    $req .= "&$key=$value";
+}
+
+// STEP 2: POST IPN data back to PayPal to validate
+
+// $ch = curl_init('https://ipnpb.sandbox.paypal.com/cgi-bin/webscr');
+$ch = curl_init('https://ipnpb.paypal.com/cgi-bin/webscr');
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+
+// In wamp-like environments that do not come bundled with root authority certificates,
+// please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set
+// the directory path of the certificate as shown below:
+// curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
+if (!($res = curl_exec($ch))) {
+    // error_log("Got " . curl_error($ch) . " when processing IPN data");
+    curl_close($ch);
+    exit;
+}
+curl_close($ch);
+
+// STEP 3: Inspect IPN validation result and act accordingly
+
+if (strcmp($res, "VERIFIED") == 0)
+// The IPN is verified, process it:
+// check whether the payment_status is Completed
+// check that txn_id has not been previously processed
+// check that receiver_email is your Primary PayPal email
+// check that payment_amount/payment_currency are correct
+// process the notification
+{
+
+    // assign posted variables to local variables
+    $item_name = $_POST['item_name'];
+    $item_number = $_POST['item_number'];
+    $payment_status = $_POST['payment_status'];
+    $payment_amount = $_POST['mc_gross'];
+    $payment_currency = $_POST['mc_currency'];
+    $txn_id = $_POST['txn_id'];
+    $receiver_email = $_POST['receiver_email'];
+    $payer_email = $_POST['payer_email'];
+
+
+    // IPN message values depend upon the type of notification sent.
+    // To loop through the &_POST array and print the NV pairs to the screen:
+    //foreach($_POST as $key => $value) {
+    //echo $key." = ". $value."<br>";}
+
+    //$conn=mysqli_connect("localhost","root","","chemesoft") or die (mysqli_connect_error());
+    $conn = mysqli_connect('chemesoftstorecom.ipagemysql.com', 'ghutch', 'Wyo=Hutch14', "chemesoft") or die(mysqli_connect_error());
+    $sql = "SELECT * FROM `upgrades` WHERE `AppID`=" . $item_number . " and `PayPalIPN`=\"\"";
+    //	echo $sql."<br>";
+    $qryresult = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_row($qryresult);
+    $upemail = $row[2];
+    //	echo $upemail."<br>";
+    $macpwd = $row[4];
+    //	echo $macpwd."<br>";
+    $sql = "UPDATE `upgrades` SET `PayPalIPN`='" . $txn_id . "', `Price`=" . $payment_amount . ", `PPEmail`='" . $payer_email . "' WHERE `AppID`=" . $item_number . " and `PayPalIPN`=\"\"";
+    //	echo $sql."<br>";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
+
+    $appname['1011'] = "Conversions";
+    $appname['2011'] = "3 Dimension Vector Math";
+    $appname['2012'] = "Cubic and Quartic Root Solver";
+    $appname['2013'] = "6x6 Matrix Math";
+    $appname['2101'] = "Two Parameter Regression";
+    $appname['2102'] = "Polynomial Regression";
+    $appname['2103'] = "Multivariable Regression";
+    $appname['3011'] = "Fluid Flow Pressure Loss";
+    $appname['3012'] = "Fluid Flow Meter Sizing";
+    $appname['4011'] = "Six Component Vap-Liq Ideal Mixture Equilibrium";
+    $appname['4012'] = "Single Component Equations of State";
+    $appname['4013'] = "Multi-Component Equations of State";
+
+    $reqa = "Thank you for the purchase of your upgrade for the " . $appname[$item_number] . " app. To upgrade, open the app and click UPGRADE, ";
+    $reqb = "enter your e-mail address (" . $upemail . ") and password." . PHP_EOL;
+    $reqc = "UPGRADE PASSWORD -  " . $macpwd . PHP_EOL . PHP_EOL;
+    $reqd = "Your PayPal charge is $" . $payment_amount;
+    $reqz = wordwrap($reqa . $reqb . $reqc . $reqd, 60);
+    //    echo $reqz;
+
+    //    Send an email to the Customer with Upgrade password
+    $mail_From    = "From: upgrades@chemesoftstore.com";
+    $mail_To      = $upemail;
+    $mail_Subject = "ChemE Soft Store App Upgrade Password";
+    $mail_Body    = $reqz;
+    mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
+
+    // Authentication protocol is complete - OK to process notification contents
+
+    // Possible processing steps for a payment include the following:
+
+    // Check that the payment_status is Completed
+    // Check that txn_id has not been previously processed
+    // Check that receiver_email is your Primary PayPal email
+    // Check that payment_amount/payment_currency are correct
+    // Process payment
+
+
+} else if (strcmp($res, "INVALID") == 0) {
+    // IPN invalid, log for manual investigation
+    //echo "The response from IPN was: <b>" .$res ."</b>";
+    // Authentication protocol is complete - begin error handling
+    $reqa = "Purchase of your upgrade for the " . $appname[$item_number] . " app was not approved by PayPal. ";
+    $reqb = "If you wish to continue, you must contact PayPal.com to resolve the issue" . PHP_EOL;
+    $reqc = "Uncompleted transaction...";
+    $reqz = wordwrap($reqa . $reqb . $reqc, 60);
+    //    echo $resz;
+
+    // Send an email announcing the IPN message is INVALID
+    $mail_From    = "upgrades@chemesoftstore.com";
+    $mail_To      = $upemail;
+    $mail_Subject = "ChemE Soft Store App Upgrade PayPal INVALID";
+    $mail_Body    = $reqz;
+
+    mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
+}
+fclose($fp);  // Close the file
--- upgrdFunc.js
+++ upgrdFunc.js
@@ -1,21 +1,73 @@
-function upgrdApp(AppID, paybool) {
-  var userEM = prompt(
-    "Please Enter E-mail address to receive PASSWORD for App Upgrade"
-  );
+function downLoad(appNum) {
+  console.log("Download script");
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "laramie",
+    database: "chemtest",
+    insecureAuth: true
+  });
+  console.log(new Date(), appNum);
+  con.connect(function(err) {
+    if (err) throw console.log(err);
+    var sql = "INSERT INTO Downloads (AppNum) VALUES ('" + appNum + "')";
+    console.log(sql);
+    con.query(sql, function(err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+    con.end();
+  });
+}
+
+function upgrdApp(appNum, paybool) {
+  var userEM = "ab.test@linkedin.com";
+  // var userEM = prompt(
+  //   "Please Enter E-mail address to receive PASSWORD for App Upgrade"
+  // );
   var filter = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!filter.test(userEM)) {
-    alert("No or INVALID E-mail Address Entered");
+    // alert("No or INVALID E-mail Address Entered");
     return false;
   } else {
     if (paybool) {
-      alert("Connecting to PayPal website for Order submittal");
+      var mysql = require("mysql");
+      var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "laramie",
+        database: "chemtest",
+        insecureAuth: true
+      });
+      con.connect(function(err) {
+        if (err) throw console.log(err);
+        var sql = "SELECT * FROM appdata WHERE AppNum = " + appNum;
+        con.query(sql, function(err, result, fields) {
+          if (err) throw err;
+          appID = result[0].AppID;
+          paidPwd = genPwd(userEM, appID);
+          console.log(appNum, appID, userEM, paidPwd);
+        });
+        // var sql = "INSERT INTO upgrades (upgrdDT, ugEmail, ugApp, ugPwd) VALUES ()" + appNum;
+        // // var appPwd = "";
+        // con.query(sql, function(err, result, fields) {
+        //   if (err) throw err;
+        //   appID = result[0].AppID;
+        //   paidPwd = genPwd(userEM, appID);
+        //   console.log(appID, paidPwd);
+        // });
+        con.end();
+      });
+      // alert("Connecting to PayPal website for Order submittal");
     } else {
-      upgrdPWD(userEM, AppID);
+      freePwd = genPwd(userEM, "G0A0");
+      console.log(freePwd);
     }
   }
 }
 
-function upgrdPWD(email, appNum) {
+function genPwd(email, appID) {
   var ugPwd = "";
   emAddr = email.toLowerCase();
   posAt = emAddr.indexOf("@");
@@ -23,46 +75,54 @@ function upgrdPWD(email, appNum) {
   emDom = emAddr.substring(posAt + 1);
   posDm = emDom.indexOf(".");
   emDomNm = emDom.slice(0, posDm);
-  if (appNum == 1011) {
-    appID = "G0A0";
-  }
 
   char1 = String.fromCharCode(
     ((emDomNm.charCodeAt(0) + emDom.length) % 26) + 65
   );
   char2 = String.fromCharCode(
-    ((emDomNm.charCodeAt(1) + emDom.length) % 10) + 48
+    ((emName.charCodeAt(0) + emName.length) % 10) + 48
   );
   char3 = String.fromCharCode(
-    ((emName.charCodeAt(0) + appID.charCodeAt(0)) % 26) + 65
+    ((emDomNm.charCodeAt(0) + appID.charCodeAt(0)) % 26) + 65
   );
   char4 = String.fromCharCode(
-    ((emName.charCodeAt(1) + appID.charCodeAt(1)) % 10) + 48
+    ((emName.charCodeAt(0) + appID.charCodeAt(1)) % 10) + 48
   );
   char5 = String.fromCharCode(
-    ((emName.charCodeAt(2) + appID.charCodeAt(2)) % 26) + 65
+    ((emDomNm.charCodeAt(1) + appID.charCodeAt(2)) % 26) + 65
   );
   char6 = String.fromCharCode(
-    ((emName.charCodeAt(3) + appID.charCodeAt(3)) % 10) + 48
+    ((emName.charCodeAt(1) + appID.charCodeAt(3)) % 10) + 48
   );
-
-  console.log(char1, char2, char3, char4, char5, char6);
   return char1 + char2 + char3 + char4 + char5 + char6;
 }
 
-function connMYSQL() {
-  var mysql = require("mysql");
-
-  var con = mysql.createConnection({
-    host: "chemesoftstorecom.ipagemysql.com",
-    user: "ghutch",
-    password: "Wyo=Hutch14",
-    port: 3308
+function sendPwd(emClient, freePwd) {
+  var nodemailer = require("nodemailer");
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "ghutchwyo14@gmail.com",
+      pass: "WyoHutch4!"
+    }
   });
-
-  con.connect(function(err) {
-    if (err) throw console.log(err);
-    console.log("Connected !");
+  var mailOptions = {
+    from: "ghutchwyo14@gmail.com",
+    to: emClient,
+    subject: "Conversions App Free Upgrade Password",
+    text:
+      "Thank you for using the Conversions app. To upgrade, open the app and click UPGRADE, " +
+      "enter your e-mail address (" +
+      emClient +
+      ") and password (" +
+      freePwd
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
   });
 }
-// connMYSQL();
+console.log(new Date());
